4037f4f57ecd6d192cff43302ca3f6cf48bb3166
kazeneza-zephilin <kzephilin2@gmail.com>
2024-08-23 10:58:18 +0200

    added start button to reset the game. refactored playround function with separate eatenBy respecting the separate of concern.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
-let isMatchEnd = false;
 let humanScore = 0;
 let computerScore = 0;
 const runningScore = document.querySelector("#score");
+runningScore.innerHTML = `<p>plyer ${humanScore} - computer ${computerScore}</p>`
 const finalScore = document.querySelector("#final-score");
-
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => playRound("rock"));
 const paper = document.querySelector("#paper");
 paper.addEventListener("click", () => playRound("paper"));
 const scissor = document.querySelector("#scissor");
 scissor.addEventListener("click", () => playRound("scissor"));
+const startBtn = document.querySelector("#start")
+startBtn.addEventListener('click', () => resetGame() )
 
 function getComputerChoice() {
   const computerInput = Math.floor(Math.random() * 10);
@@ -29,52 +30,49 @@ function getStringChoice() {
 }
 
 function playRound(hummanChoice, computerChoice) {
-    if (humanScore === 5 || computerScore === 5){
-        isMatchEnd = true;
-        return
-    }
+  if (humanScore === 5){
+    logMessage(finalScore, "😂🎉You Win")
+    return
+  }else if (computerScore === 5){
+    logMessage(finalScore, "😚You Lose")
+    return;
+  }
+
   computerChoice = getStringChoice();
   if (computerChoice === hummanChoice) {
     console.log("Deduce\n");
-  } else if (eatenBy()) {
+  } else if (eatenBy(hummanChoice, computerChoice)) {
     console.log("Player Win \n");
     humanScore += 1;
   } else {
     console.log("Computer win \n");
     computerScore += 1;
   }
-  console.log("Score Board");
-  console.log(`Computer score ${computerScore}`);
-  console.log(`Human score ${humanScore}`);
-  runningScore.textContent = `player score ${humanScore} - computer score ${computerScore}`;
-
-  function eatenBy() {
-    if (computerChoice === "rock" && hummanChoice === "paper") {
-      return true;
-    } else if (computerChoice === "rock" && hummanChoice === "scissor") {
-      return false;
-    } else if (computerChoice === "paper" && hummanChoice === "scissor") {
-      return true;
-    } else if (computerChoice === "paper" && hummanChoice === "rock") {
-      return false;
-    } else if (computerChoice === "scissor" && hummanChoice === "rock") {
-      return true;
-    } else if (computerChoice === "scissor" && hummanChoice === "paper") {
-      return false;
-    }
-  }
+  runningScore.innerHTML = `<p> player ${humanScore} computer ${computerScore} </p>`;
 }
-console.log(isMatchEnd)
-if (isMatchEnd){
-    if (humanScore > computerScore){
-        logMessage(finalScore, "You win the game")
-    }else if (humanScore < computerScore){
-        logMessage(finalScore, "You lose the game")
-    }else{
-        logMessage(finalScore, "match ended in tie")
-    }
+function eatenBy(hummanChoice, computerChoice) {
+  if (computerChoice === "rock" && hummanChoice === "paper") {
+    return true;
+  } else if (computerChoice === "rock" && hummanChoice === "scissor") {
+    return false;
+  } else if (computerChoice === "paper" && hummanChoice === "scissor") {
+    return true;
+  } else if (computerChoice === "paper" && hummanChoice === "rock") {
+    return false;
+  } else if (computerChoice === "scissor" && hummanChoice === "rock") {
+    return true;
+  } else if (computerChoice === "scissor" && hummanChoice === "paper") {
+    return false;
+  }
 }
 
 function logMessage(elem, message) {
-  elem.textContent = `<p>${message}</p>`;
+  elem.innerHTML = `<p>${message}</p>`;
+}
+
+function resetGame(){
+  humanScore = 0;
+  computerScore = 0;
+  runningScore.innerHTML = `<p>plyer ${humanScore} - computer ${computerScore}</p>`
+  finalScore.textContent = '';
 }
